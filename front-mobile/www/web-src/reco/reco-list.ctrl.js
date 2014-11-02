23a9ebc0502a21d23770f0c10fd659d6d0bf86ea
Dmytro Podyachiy <dimapog@gmail.com>
2014-11-02 14:31:04 +0100

    Front-mobile: Card style + Delete Option button (+animation)

--- front-mobile/www/web-src/reco/reco-list.ctrl.js
+++ front-mobile/www/web-src/reco/reco-list.ctrl.js
@@ -3,20 +3,30 @@ angular.module('itrade-mobile').controller('recoListCtrl', function ($scope, $ti
         return new Date(new Date().getTime() + Math.floor( Math.random()*5000000000 ) );
     };
     $scope.recos = [
-        {id: "7b9057b4", ticker: 'AAPL', when: generateDateInFuture(), pt: 112, confidence: 67, followed: true, followers:873, starred: true, title: 'Apple' },
-        {id: "e1aa45da", ticker: 'AMZN', when: generateDateInFuture(), pt: 86,  confidence: 19, followed:false, followers:112, starred:false, title: 'Amazon' },
-        {id: "c9dd77e3", ticker: 'GOOG', when: generateDateInFuture(), pt: 1021,confidence: 93, followed: true, followers:542, starred:false, title: 'Google' },
-        {id: "06a80382", ticker: 'MSFT', when: generateDateInFuture(), pt: 82,  confidence: 32, followed:false, followers:  2, starred:false, title: 'Microsoft' },
-        {id: "55f1d3f3", ticker: 'CSCO', when: generateDateInFuture(), pt: 33,  confidence:  9, followed: true, followers: 23, starred: true, title: 'Cisco' },
-        {id: "692ca0a5", ticker: 'FB'  , when: generateDateInFuture(), pt: 183, confidence: 56, followed: true, followers:199, starred:false, title: 'Facebook' }
+        {id: "7b9057b4", ticker: 'AAPL', when: generateDateInFuture(), recoPrice: 112,  priceTarget: 112, confidence: 67, followed: true, followers:873, starred: true, title: 'Apple' },
+        {id: "e1aa45da", ticker: 'AMZN', when: generateDateInFuture(), recoPrice: 86,   priceTarget: 86,  confidence: 19, followed:false, followers:112, starred:false, title: 'Amazon' },
+        {id: "c9dd77e3", ticker: 'GOOG', when: generateDateInFuture(), recoPrice: 1021, priceTarget: 1021,confidence: 93, followed: true, followers:542, starred:false, title: 'Google' },
+        {id: "06a80382", ticker: 'MSFT', when: generateDateInFuture(), recoPrice: 82,   priceTarget: 82,  confidence: 32, followed:false, followers:  2, starred:false, title: 'Microsoft' },
+        {id: "55f1d3f3", ticker: 'CSCO', when: generateDateInFuture(), recoPrice: 33,   priceTarget: 33,  confidence:  9, followed: true, followers: 23, starred: true, title: 'Cisco' },
+        {id: "692ca0a5", ticker: 'FB'  , when: generateDateInFuture(), recoPrice: 183,  priceTarget: 183, confidence: 56, followed: true, followers:199, starred:false, title: 'Facebook' }
     ];
 
     $scope.doRefresh = function() {
         console.log('Refreshing!');
         $timeout( function() {
             //simulate async response
-            var id = Math.floor(Math.random() * 1000) + 4;
-            $scope.recos.unshift({title: 'New Item ' + id, id: id });
+            $scope.recos.unshift({
+                id: Math.random().toString(16).substr(2),
+                ticker: 'New ticker',
+                when: generateDateInFuture(),
+                recoPrice: 112,
+                priceTarget: 112,
+                confidence: 67,
+                followed: true,
+                followers:873,
+                starred: true,
+                title: 'Title'
+            });
             //Stop the ion-refresher from spinning
             $scope.$broadcast('scroll.refreshComplete');
         }, 1000);
@@ -31,8 +41,13 @@ angular.module('itrade-mobile').controller('recoListCtrl', function ($scope, $ti
 
         reco.selected = !oldValue;
     };
+
     $scope.toggleFollow = function(reco, $event) {
         $event.preventDefault();
         reco.followed = !reco.followed;
+    };
+
+    $scope.remove = function(reco) {
+        _.remove($scope.recos, reco);
     };
 });

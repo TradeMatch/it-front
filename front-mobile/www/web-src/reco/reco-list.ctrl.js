9dc0d8297de1c90d768328ad70c524aaf71e0c9c
Dmytro Podyachiy <dimapog@gmail.com>
2014-11-02 22:23:11 +0100

    Front-mobile: Extended reco

--- front-mobile/www/web-src/reco/reco-list.ctrl.js
+++ front-mobile/www/web-src/reco/reco-list.ctrl.js
@@ -3,12 +3,12 @@ angular.module('itrade-mobile').controller('recoListCtrl', function ($scope, $ti
         return new Date(new Date().getTime() + Math.floor( Math.random()*5000000000 ) );
     };
     $scope.recos = [
-        {id: "7b9057b4", ticker: 'AAPL', when: generateDateInFuture(), recoPrice: 112,  priceTarget: 112, confidence: 67, followed: true, followers:873, starred: true, title: 'Apple' },
-        {id: "e1aa45da", ticker: 'AMZN', when: generateDateInFuture(), recoPrice: 86,   priceTarget: 86,  confidence: 19, followed:false, followers:112, starred:false, title: 'Amazon' },
-        {id: "c9dd77e3", ticker: 'GOOG', when: generateDateInFuture(), recoPrice: 1021, priceTarget: 1021,confidence: 93, followed: true, followers:542, starred:false, title: 'Google' },
-        {id: "06a80382", ticker: 'MSFT', when: generateDateInFuture(), recoPrice: 82,   priceTarget: 82,  confidence: 32, followed:false, followers:  2, starred:false, title: 'Microsoft' },
-        {id: "55f1d3f3", ticker: 'CSCO', when: generateDateInFuture(), recoPrice: 33,   priceTarget: 33,  confidence:  9, followed: true, followers: 23, starred: true, title: 'Cisco' },
-        {id: "692ca0a5", ticker: 'FB'  , when: generateDateInFuture(), recoPrice: 183,  priceTarget: 183, confidence: 56, followed: true, followers:199, starred:false, title: 'Facebook' }
+        {selected: true, id: "7b9057b4", ticker: 'AAPL', when: generateDateInFuture(), recoPrice: 112,  priceTarget: 112, confidence: 67, followed: true, followers:873, consensus: 87, risk: 75, momentum: 46, starred: true, title: 'Apple' },
+        {id: "e1aa45da", ticker: 'AMZN', when: generateDateInFuture(), recoPrice: 86,   priceTarget: 86,  confidence: 19, followed:false, followers:112, starred:false, consensus: 87, risk: 75, momentum: 46, title: 'Amazon' },
+        {id: "c9dd77e3", ticker: 'GOOG', when: generateDateInFuture(), recoPrice: 1021, priceTarget: 1021,confidence: 93, followed: true, followers:542, starred:false, consensus: 87, risk: 75, momentum: 46, title: 'Google' },
+        {id: "06a80382", ticker: 'MSFT', when: generateDateInFuture(), recoPrice: 82,   priceTarget: 82,  confidence: 32, followed:false, followers:  2, starred:false, consensus: 87, risk: 75, momentum: 46, title: 'Microsoft' },
+        {id: "55f1d3f3", ticker: 'CSCO', when: generateDateInFuture(), recoPrice: 33,   priceTarget: 33,  confidence:  9, followed: true, followers: 23, starred: true, consensus: 87, risk: 75, momentum: 46, title: 'Cisco' },
+        {id: "692ca0a5", ticker: 'FB'  , when: generateDateInFuture(), recoPrice: 183,  priceTarget: 183, confidence: 56, followed: true, followers:199, starred:false, consensus: 87, risk: 75, momentum: 46, title: 'Facebook' }
     ];
 
     $scope.doRefresh = function() {
